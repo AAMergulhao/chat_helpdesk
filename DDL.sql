9a67e9839231c5b3887e561ea61aa416107c5d8a
AAMergulhao <alves.m.arthur@gmail.com>
2020-09-22 01:22:25 +0000

    #Sprint_2 Alteração no banco de dados para o nome oficial do projeto

--- DDL.sql
+++ DDL.sql
@@ -1,10 +1,10 @@
-create schema anotacao;
+create schema chatbot_helpdesk;
 
-use anotacao;
+use chatbot_helpdesk;
 
 create user 'user'@'localhost' identified by 'pass123';
 
-grant select, insert, delete, update on anotacao.* to user@'localhost';
+grant select, insert, delete, update on chatbot_helpdesk.* to user@'localhost';
 
 create table usr_usuario (
   usr_id bigint unsigned not null auto_increment,
