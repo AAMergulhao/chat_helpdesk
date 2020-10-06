94f96c30f28688a8a63f2d6c0dc88a5a8e5ec6e3
Arthur Mergulhão <alves.m.arthur@gmail.com>
2020-10-06 08:18:15 -0300

    Formatação dos retornos no arquivo ChatController

--- src/main/java/br/gov/sp/fatec/springbootapp/controller/ChatController.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/controller/ChatController.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.gov.sp.fatec.springbootapp.entity.Conversa;
 import br.gov.sp.fatec.springbootapp.entity.Mensagem;
@@ -33,22 +37,35 @@ public class ChatController {
 
     @JsonView({View.ConversaResumo.class})
     @PostMapping(value = "/iniciarConversa")
-    public Conversa iniciarConversa(@RequestBody ObjectNode body) {
+    public ResponseEntity<Conversa> iniciarConversa(@RequestBody ObjectNode body, UriComponentsBuilder uriComponentsBuilder) {
         String nomeRemetente = body.get("nomeRemetente").asText();
         String nomeDestinatario = body.get("nomeDestinatario").asText();
         
         Conversa conversa = chatService.iniciarConversa(nomeRemetente, nomeDestinatario, body.get("dataHora").asText(), body.get("conteudo").asText());
-        return conversa;
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(
+            uriComponentsBuilder.path(
+                "/chat/iniciarConversa/"+ conversa.getId()).build().toUri());
+
+        return new ResponseEntity<Conversa>(conversa, responseHeaders, HttpStatus.CREATED);
     }
 
     @JsonView(View.MensagemResumo.class)
     @PostMapping(value = "/enviarMensagem")
-    public Mensagem enviarMensagem(@RequestBody ObjectNode body) {
+    public ResponseEntity<Mensagem> enviarMensagem(@RequestBody ObjectNode body, UriComponentsBuilder uriComponentsBuilder) {
         String nomeRemetente = body.get("nomeRemetente").asText();
         String nomeDestinatario = body.get("nomeDestinatario").asText();
-        
+
         Mensagem mensagem = chatService.enviarMensagem(body.get("chatID").asLong(), nomeRemetente, nomeDestinatario, body.get("dataHora").asText(),body.get("conteudo").asText());
-        return mensagem;
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(
+            uriComponentsBuilder.path(
+                "/chat/enviarMensagem/"+ mensagem.getId()).build().toUri());
+        
+        
+        return new ResponseEntity<Mensagem>(mensagem, responseHeaders, HttpStatus.CREATED);
     }
 
     @JsonView(View.MensagemResumo.class)
@@ -61,10 +78,16 @@ public class ChatController {
 
     @JsonView(View.MensagemResumo.class)
     @GetMapping(value = "/buscarMensagens")
-    public Set<Mensagem> buscarMensagensConversa(@RequestParam("conversaID") String id) {
+    public ResponseEntity<Set<Mensagem>> buscarMensagensConversa(@RequestParam("conversaID") String id , UriComponentsBuilder uriComponentsBuilder) {
         
         Set<Mensagem> mensagens = chatService.buscarMensagensPorIdConversa(Long.parseLong(id));
-        return mensagens;
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(
+            uriComponentsBuilder.path(
+                "/chat/enviarMensagem/"+ mensagens.iterator().next().getId()).build().toUri());
+
+        return new ResponseEntity<Set<Mensagem>>(mensagens, responseHeaders, HttpStatus.CREATED);
     }
 
     
