f0f32dd1aca84772cf073d7bc051138950b52fc9
RafaelRez75 <rrezende75@gmail.com>
2020-10-20 13:44:45 +0000

    Aula Spring Security 2

--- src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,7 +46,7 @@ public class SegurancaServiceImpl implements SegurancaService{
     }
 
     @Override
-    @Transactional
+    @PreAuthorize("isAuthenticated()")
     public List<Usuario> buscarTodosUsuarios(){
         return usuarioRepo.findAll();
     }
